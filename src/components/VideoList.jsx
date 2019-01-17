729346a04bb9de26187124ca9596431c78067f3e
Cowie22 <rjcowie@ucla.edu>
2019-01-16 21:50:10 -0800

    -m began to get working implementation for videolist, videolistentry, and videoplayer

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,30 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+// import exampleVideoData from "../data/exampleVideoData.js";
+import VideoListEntry from './VideoListEntry.js';
+
+
+var VideoList = (props) => {
+  var rows = [];
+  for (let i = 0; i < props.videos.length; i++) {
+    var video = props.videos[i];
+    rows.push( 
+      <VideoListEntry video={video}/>
+    )
+  }
+
+  return (
+    <div className="video-list"> 
+      {rows}
+    </div>
+  )
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
 };
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
